891eacb914dd39ff05959a9f7fdd0205e99184b7
Skeo <skeo051600@gmail.com>
2024-09-15 11:21:12 -0500

    Fix create band profile issue Adjusted data sets. Fixed issues with forms not correct utilizing user profile data.
    	modified:   api/bandsData.js
    	new file:   api/rolesData.js
    	modified:   components/BandCard.js
    	new file:   components/BandRoleCard.js
    	new file:   pages/bandForm.js
    	modified:   pages/bands.js
    	modified:   styles/globals.css
    	modified:   utils/sample-data/rolesList.json

---
+++ components/BandRoleCard.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { deleteRole, getSingleRole } from '../api/rolesData';
+
+// function RoleSelect({ roleObj, onUpdate }) {
+//   const deleteThisRole = () => {
+//     getSingleRole(roleObj.band_role_id).then((role) => {
+//       if (window.confirm('Delete this role?')) {
+//         deleteRole(roleObj.band_role_id).then(() => onUpdate());
+//       }
+//     });
+//   };
+// }
+
+// export default RoleSelect;
